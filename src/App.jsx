5233445d6e0a8edf088986ab423baa7326824e83
kelvinmacharia254 <pilotkelvin0@gmail.com>
2024-10-02 21:32:56 +0300

    add notification component, redux thunk and notification state to redux store

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,18 @@ import './App.css'
 import Layout from "./components/Layout/Layout.jsx";
 import Cart from "./components/Cart/Cart.jsx";
 import Products from "./components/shop/Products.jsx";
+import Notification from "./components/UI/Notification.jsx";
 
 import {useSelector} from "react-redux";
 function App() {
     const cartIsVisible = useSelector((state) => state.ui.cartIsVisible);
   return (
     <>
+        <Notification
+            status = "pending"
+            title= "sending..."
+            message = "sending cart data"
+        />
       <Layout>
           {cartIsVisible && <Cart/>}
         <Products/>
